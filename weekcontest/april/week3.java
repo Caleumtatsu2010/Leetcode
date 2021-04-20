a5d15a859d42e0693ebe0821697b868f55c7e220
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2021-04-20 21:46:33 +0700

    add combinationSum4
    add combinationSum4 in week3

--- weekcontest/april/week3.java
+++ weekcontest/april/week3.java
@@ -1,7 +1,14 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-
+class ListNode 
+{
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 public class week3
 {
     public static String removeDuplicates(String s, int k) 
@@ -31,11 +38,34 @@ public class week3
     public static int numSubmatrixSumTarget(int[][] matrix, int target) 
     {
         
-        
+        return 0;
+    }
+    // public static ListNode removeNthFromEnd(ListNode head, int n) 
+    // {
+    
+    // }
+    public static int combinationSum4(int[] nums, int target) 
+    {
+        if(target == 0)
+        {
+            return 1;
+        }
+        int count =0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] <= target)
+            {
+                count = count + combinationSum4(nums, target-nums[i]);
+                System.out.println(count);
+            }
+        }
+        return count;
     }
     public static void main(String[] args) 
     {
-        System.out.println(removeDuplicates("deeedbbcccbdaa", 3));
+        //System.out.println(removeDuplicates("deeedbbcccbdaa", 3));
+        int []nums = new int[]{1,2,3};
+        System.out.println(combinationSum4(nums, 3));
         
         
     }
