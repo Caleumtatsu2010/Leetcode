e5dfeb1b2cca40040ad9c6796d6318051adce534
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2021-04-28 23:04:31 +0700

    add furthestBuilding

--- weekcontest/april/week4.java
+++ weekcontest/april/week4.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class week4 
 {
@@ -75,17 +76,38 @@ public class week4
             }
             n = n/3;
         }
-        if(n==1)
-        {
-            return true;
-        }
-            
+        return n==1;
     }
+    public static int furthestBuilding(int[] h, int b, int l) 
+    {
+        PriorityQueue<Integer> min_heap = new PriorityQueue<>();
+        for(int i=0;i<h.length-1;i++)
+        {
+            int diff = h[i+1] - h[i]; 
+            if(diff > 0)
+            {
+                min_heap.add(diff);
+            }//use bricks first for small distance
+            if(min_heap.size() > l)
+            {
+                b = b - min_heap.remove();
+            }
+            //if heapmin.size() == ladder => we can pass more heapmin.size() builings
+            if(b<0)
+            {
+                return i;
+            }
+        }
+        return h.length - 1;
+    }
+
     public static void main(String[] args) {
         
         //System.out.println(countBinarySubstrings("00110011"));
         // int [][]matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
         // rotate(matrix);
-        System.out.println(isPowerOfThree(2));
+        //System.out.println(isPowerOfThree(2));
+        int []h = {4,12,2,7,3,18,20,3,19};
+        System.out.println(furthestBuilding(h, 10, 2));
     }
 }
