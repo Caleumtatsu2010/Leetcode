41b9d0ffdee80fabdc003ce5de62004c03317511
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-09-17 20:34:36 +0700

    add maxTurbulenceSize

--- weekcontest/2021/september/week3.java
+++ weekcontest/2021/september/week3.java
@@ -1,20 +1,25 @@
-public class week3 {
-        public static int maxTurbulenceSize(int[] A) {
-            int N = A.length;
-            int ans = 1;
-            int anchor = 0;
-    
-            for (int i = 1; i < N; ++i) {
-                int c = Integer.compare(A[i-1], A[i]);
-                if (c == 0) {
-                    anchor = i;
-                } else if (i == N-1 || c * Integer.compare(A[i], A[i+1]) != -1) {
-                    ans = Math.max(ans, i - anchor + 1);
-                    anchor = i;
+public class week3 
+{
+    public static int maxTurbulenceSize(int[] A) 
+    {
+            int mx = 0, c = 0;
+            
+            for(int i = 0; i < A.length; i++) {
+                if(i >= 2 && ((A[i-2] > A[i-1] && A[i-1] < A[i]) || (A[i-2] < A[i-1] && A[i-1] > A[i])) 
+                {
+                    c++;
+                } 
+                else if(i >= 1 && A[i-1] != A[i]) {
+                    
+                    c = 2;
+                }
+                else {
+                    
+                    c = 1;
                 }
             }
-    
-            return ans;
+            return Math.max(mx, c);
+            }
     }
     public static void main(String[] args) {
         
