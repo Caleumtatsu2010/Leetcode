74277f003a067dce97285623df002cda6326e5c0
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-09-17 22:38:21 +0700

    Update Problem9.java

--- leetproblem/Problem9.java
+++ leetproblem/Problem9.java
@@ -33,10 +33,40 @@ public class Problem9 {
         return count;
         
     }
+    public static void quickSort(int x[], int left, int right)
+    {
+        if (left < right) 
+        {
+            int k = (left + right) / 2; 
+            int t = x[k];
+            int i = left; int 
+            j = right;
+            do
+            {
+                while (x[i] < t) i = i + 1;
+                while (x[j] > t) j = j - 1;
+                if (i <= j) 
+                {
+                    int tg = x[i];
+                    x[i] = x[j]; x[j] = tg;
+                    i = i+1; j = j-1;
+                }
+            }while (i <= j);
+            quickSort(x,left,j);
+            quickSort(x,i,right);
+        }
+    }
+    public static int[] sortArray(int[] nums)
+    {
+        quickSort(nums, 0, nums.length-1);
+        return nums;
+
+    }
     public static void main(String[] args) {
-        //int []nums = {9,2,9};
+        int []nums = {9,2,9,5,3,8,6};
         //System.out.println(Arrays.toString(plusOne(nums)));
-        System.out.println(mySqrt(25));
+        //System.out.println(mySqrt(25));
+        System.out.println(Arrays.toString(sortArray(nums)));
         
     }
     
