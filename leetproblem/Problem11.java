15c631c1c04b41138da8469b68738f2a63cd227f
LongNH02 <longnh02@ominext.com>
2022-03-14 23:07:15 +0700

    preorder traversal

--- leetproblem/Problem11.java
+++ leetproblem/Problem11.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+class Node
+{
+    int val;
+    Node left, right;
+ 
+    public Node(int val)
+    {
+        this.val = val;
+        left = right = null;
+    }
+}
+
 public class Problem11 {
     public static boolean validMountainArray(int[] arr) {
         if(arr.length <3)
@@ -15,9 +29,49 @@ public class Problem11 {
         else
             return false;
     }
-    public static void main(String[] args) {
-        int arr[] = new int[]{0,3,2,1};
-        System.out.println(validMountainArray(arr));
+    static void readNode(Node root)
+    {
+        if(root==null)
+            return;
+        System.out.print(root.val);
+        readNode(root.left);
+        readNode(root.right);
     }
-    
+
+
+    public static void main(String[] args) {
+        // int arr[] = new int[]{0,3,2,1};
+        // System.out.println(validMountainArray(arr));
+
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.left.right.left = new Node(7);
+        tree.root.left.right.right = new Node(8);
+        tree.root.right = new Node(3);
+        tree.root.right.right = new Node(6);
+        tree.root.right.right.left = new Node(9);
+
+        
+        readNode(tree.root);
+
+
+
+    }   
 }
+class BinaryTree{
+
+    Node root;
+    BinaryTree(int key)
+    {
+        root = new Node(key);
+    }
+ 
+    BinaryTree()
+    {
+        root = null;
+    }
+}
+
